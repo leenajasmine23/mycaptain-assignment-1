7bb3aaa572227e208c529b7f632b60ef2337eff2
Leenajasmine21 <86966877+Leenajasmine21@users.noreply.github.com>
2021-07-21 11:42:03 +0530

    created Python program.py

---
+++ Python program.py
@@ -0,0 +1,10 @@
+import math
+radius = float(input(*input the radius of the circle: *))
+area = math.pi * radius ** 2
+printf("the area of the circle with radius" +str(radius)+" is:" +str(area))
+
+
+
+filename = input("Input the filename:")
+f_extns = filename.split(".")
+print("the extension of the file is :"+ repr(f_extns[-1]))
